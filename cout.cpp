5ec2da1c6bf342c305081bac43a6059e88a93363
BoBoya <b02901124@ntu.edu.tw>
2016-05-21 16:52:28 +0800

    reset

---
+++ cout.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void printBigSquare(){
+	cout<<"algorithm is GGing."<<endl<<endl;
+}
+void printcuemao(){
+	cout<<"maooooo"<<endl;
+}
+void printhuanchin(){
+	cout<<"huanchin";
+}
+void printBoBoya(){
+	cout<<"BoBoHuang is me , I print my name~~"<<endl;
+	cout<<endl<<endl;
+	for(int i=1; i<4; i++){
+		cout<<"the "<<i<<" time iteration"<<endl;
+	}
+}
+
+
+
+int main()
+{
+	printBigSquare();
+	printcuemao();
+	printhuanchin();
+	printBoBoya();
+	return 0;
+}
