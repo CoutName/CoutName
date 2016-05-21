656652d3b2496a8a683132f3c638463af058a847
BoBoya <b02901124@ntu.edu.tw>
2016-05-21 15:41:05 +0800

    Create main.cpp to Cout our name

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void printBigSquare(){
+}
+void printcuemao(){
+}
+void printhuanchin(){
+}
+void printBoBoya(){
+}
+
+
+
+int main()
+{
+	printBigSquare();
+	printcuemao();
+	printhuanchin();
+	printBoBoya();
+	return 0;
+}
